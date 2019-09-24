e383f2313c43ab0fc6709442679c629dc22da299
Antton Jokinen <antton.jokinen@gmail.com>
2019-09-24 14:41:28 +0300

    Add extern C defining

--- SLVN/include/core.h
+++ SLVN/include/core.h
@@ -7,6 +7,14 @@
 	#define SLVN_API __declspec(dllimport)
 #endif
 
+#ifdef __cplusplus
+#define SLVN_EXTERN_BEGIN extern "C" {
+#define SLVN_EXTERN_END }
+#else
+#define SLVN_EXTERN_BEGIN
+#define SLVN_EXTERN_END
+#endif
+
 #define SLVN_RESULT int
 #define SLVN_OK 0
 #define SLVN_UNEXPECTED_RESULT -1
--- SLVN/include/window.h
+++ SLVN/include/window.h
@@ -15,11 +15,12 @@ typedef struct
 	WNDCLASSW wc;
 } slvn_window;
 
+SLVN_EXTERN_BEGIN
 SLVN_RESULT		SLVN_API	slvn_window_initialize(slvn_window* window, int width, int height);
 SLVN_RESULT		SLVN_API	slvn_window_update(slvn_window* window);
 void			SLVN_API	slvn_window_wait(slvn_window* window);
 void			SLVN_API	slvn_window_poll(slvn_window* window);
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT			CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+SLVN_EXTERN_END
 
 #endif 
